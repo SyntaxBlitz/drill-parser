d9c973f7e958c0a7c27e9181ea42f8aebd598e89
Timothy Aveni <timothy@tacosareawesome.com>
2014-08-27 09:05:43 -0400

    Output JSON

--- parse.rb
+++ parse.rb
@@ -1,4 +1,5 @@
 require 'pdf/reader'
+require 'json'
 
 class CustomPageTextReceiver < PDF::Reader::PageTextReceiver
   attr_reader :characters, :mediabox
@@ -12,11 +13,19 @@ reader = PDF::Reader.new(ARGV[0])
 
 text_receiver = CustomPageTextReceiver.new
 
+spots = []
+
 reader.pages.each do |page|
 	unless page.nil?
 		page.walk(text_receiver)
 		runs = CustomPageLayout.new(text_receiver.characters, text_receiver.mediabox).runs
 
 		runs = runs.select {|run| run.text == "p"}
+		runCoordinates = runs.map {|run| {:x => run.x, :y => run.y}}
+		spots << runCoordinates
 	end
+end
+
+File.open(ARGV[0] + ".json", 'w') do |file|
+	file.write(JSON.generate(spots))
 end
